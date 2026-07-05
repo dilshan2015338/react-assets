81075f6a600a02050d55bf6e38c396aa7185cb8f
dilshan2015338 <59525727+dilshan2015338@users.noreply.github.com>
2026-07-05 15:22:03 +0530

    Add CartContext and CartProvider for cart management

---
+++ CartContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from 'react';
+import { useCart } from '../hooks/useCart';
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const cartHook = useCart();
+    return (
+        <CartContext.Provider value={cartHook}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export const useCartContext = () => {
+    const context = useContext(CartContext);
+    if (!context) {
+        throw new Error('useCartContext must be used within CartProvider');
+    }
+    return context;
+};
